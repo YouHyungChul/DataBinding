5d2b5d15f5c7ffea469a51b7525edef522654fef
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-17 23:12:48 +0900

    OnClick 연습
    binding.~~~.setOnClick~~~~(new ONClick~~~(){
         public void onClick(View v) {
              binding.~~~();
         }
    });

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.databinding.databinding.ActivityMainBinding;
 
@@ -16,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+       binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               binding.loginBtn.setText("Login");
+           }
+       });
 
+       binding.goToGoogleMaps.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               binding.goToNaver.setText("Naver");
+           }
+       });
 
+       binding.goToNaver.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               binding.goToGoogleMaps.setText("Google");
+           }
+       });
     }
 }
