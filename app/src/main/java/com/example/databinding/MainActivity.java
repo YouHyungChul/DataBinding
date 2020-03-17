1ea38bf5939133bfad472848a6e458a21ef4c230
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-17 23:01:11 +0900

    MainActivity.java에 binding null로 만들어주고 binding 오버라이딩

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+
     }
 }
